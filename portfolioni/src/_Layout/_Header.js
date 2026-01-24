d5e4e27f59eb0a314124e95b06d9d029b938bb96
Bulldog2910 <mat.nik.jorgensen@gmail.com>
2026-01-24 17:26:20 +0100

    install react and play around
    Initialize the project, install all packages, learn how react works.

---
+++ portfolioni/src/_Layout/_Header.js
@@ -0,0 +1,6 @@
+import './_Header.css'
+function _Header(){
+    return (<div id='_Header'><p>Mathias José Bull Jørgensen</p></div>);
+}
+
+export default _Header;
---
+++ portfolioni/src/frontPage/myProjects.js
@@ -0,0 +1,16 @@
+import './myProjects.css'
+function MyProjects(){
+    return (
+        <div class='schoolProjectsGrid'>
+            <div class='gridObject'>
+                <h1 class='object'>PHP Chatbot</h1>
+                <img src='logo.svg' width='auto' height='auto'></img> {/* fix this */}
+            </div>
+            <div class='gridObject'>
+                <h1 class='object'>Kartverket project</h1>
+            </div>
+        </div>
+    );
+}
+
+export default MyProjects;
